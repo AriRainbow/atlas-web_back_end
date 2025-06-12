27441ba067a672c4f42789ac79163772b7d607ba
AriRainbow <mariel.williams@atlasschool.com>
2025-06-12 11:45:53 -0500

    use task and task2 for lint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
+  console.log(task, task2);
 
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    console.log(task,task2);
   }
 
   return [task, task2];
