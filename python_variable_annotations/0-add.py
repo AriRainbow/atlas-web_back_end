d6d33eedcf3ab7e595d9333f10b3a5e2d436d4b5
AriRainbow <mariel.williams@atlasschool.com>
2025-09-02 15:31:05 -0500

    Pycodestyle validation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 """Basic function that adds two floats with type annotations."""
+"""TEST"""
+"""if __name__ == "__main__":
+    print(add(1.11, 2.22))"""
 
 
 def add(a: float, b: float) -> float:
     """Return the sum of two floats."""
     return a + b
 
-"""TEST"""
-"""if __name__ == "__main__":
-    print(add(1.11, 2.22))"""
