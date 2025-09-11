7ae307b9d30be7f1222d5060dbf1635f5fbe25ca
AriRainbow <mariel.williams@atlasschool.com>
2025-09-11 05:37:09 -0500

    1. Async Comprehensions

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module provides a coroutine that gathers random numbers using async comprehension."""
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collect 10 random floats asynchonously using an async comprehension
+    over the async_generator coroutine, and return the values as a list.
+    """
+    return [i async for i in async_generator()]
