985d0f4a0590c0cbed639389eb779977f46734f8
AriRainbow <mariel.williams@atlasschool.com>
2025-09-11 05:57:14 -0500

    Pycodestyle validation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,10 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random floats between 0 and 10 asynchronously, pausing 1 second between each."""
+    """
+    Yield 10 random floats between 0 and 10 asynchronously,
+    pausing 1 second between each.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
