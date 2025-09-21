3470ed065a6df63a4cd9bd7179530f708bb9c708
AriRainbow <mariel.williams@atlasschool.com>
2025-09-21 18:18:03 -0500

    4. MRU Caching

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" 4. MRU caching system """
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache discards the most recently used item
+    when the cache exceed MAX_ITEMS.
+    """
+
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache and update its usage.
+        If the cache is full, discard the most recently used item.
+        """
+        if key is None or item is None:
+            return
+       
+        if key in self.cache_data:
+            # Remove key to update its position
+            del self.cache_data[key]
+
+        self.cache_data[key] = item  # MRU goes to end
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # MRU = pop the end
+            last_key, _ = self.cache_data.popitem(last=True)
+            print(f"DISCARD: {last_key}")
+
+    def get(self, key):
+        """
+        Retieve item and update its usage.
+        If not found, return None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+       
+        # Update usage to mark it as most recently used
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
