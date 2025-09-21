745c11535abe82e1e57ccdf896a5f9e86c083c12
AriRainbow <mariel.williams@atlasschool.com>
2025-09-21 17:32:26 -0500

    2. LIFO Caching

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" 2. LIFO caching system """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache is a caching system that evicts the
+    most recently added item when the cache exceeds its limit.
+    """
+
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.key_stack = []  # List to track insertion order
+
+    def put(self, key, item):
+        """
+        Add an item usinf LIFO eviction if needed.
+        Discard the last inserted item if cache is full.
+        """
+        if key is None or item is None:
+            return
+       
+        if key in self.cache_data:
+            # Remove key to update its position in stack
+            self.key_stack.remove(key)
+        self.cache_data[key] = item
+        self.key_stack.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # LIFO = pop the last inserted key
+            last_key = self.key_stack.pop(-2)  # Remove second-to-last seince latest was just added
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
+
+    def get(self, key):
+        """
+        Retrieve item by key from cache.
+        Return None if key is missing or None.
+        """
+        return self.cache_data.get(key, None)
+    
