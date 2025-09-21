bbe681d5737409fc95ee7110efc13f630093e231
AriRainbow <mariel.williams@atlasschool.com>
2025-09-21 18:28:52 -0500

    Discard BEFORE adding

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -28,12 +28,12 @@ class MRUCache(BaseCaching):
             # Remove key to update its position
             del self.cache_data[key]
 
-        
         # MRU discard logic BEFORE adding new item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # MRU = pop the end
-            last_key, _ = self.cache_data.popitem(last=True)
-            print(f"DISCARD: {last_key}")
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Get the last insert/used key
+            mru_key = next(reversed(self.cache_data))
+            self.cache_data.pop(mru_key)
+            print(f"DISCARD: {mru_key}")
 
         self.cache_data[key] = item  # MRU goes to end
 
