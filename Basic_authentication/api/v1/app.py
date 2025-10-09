a4801d3c8b0a231a192175ab0971e572d9c229a4
AriRainbow <mariel.williams@atlasschool.com>
2025-10-09 11:38:32 -0500

    Move error handlers up

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -20,14 +20,17 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """JSON response for 401 Unauthorized errors"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Not found handler """
+    return jsonify({"error": "Unauthorized"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
-
-@app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """JSON response for 401 Unauthorized errors"""
-    return jsonify({"error": "Unauthorized"}), 401
