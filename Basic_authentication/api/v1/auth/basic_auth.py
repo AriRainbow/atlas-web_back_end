d4db892ffd119ce01855bc6e7dba6a2873644c9b
AriRainbow <mariel.williams@atlasschool.com>
2025-10-12 17:10:09 -0500

    6. Basic auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for API authentication using Basic Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class inherits from Auth - for Basic Authentication"""
+    pass
