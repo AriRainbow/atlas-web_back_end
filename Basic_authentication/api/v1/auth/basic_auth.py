ad796fbcafc38283729b01d4ac31dd06cdbdb56d
AriRainbow <mariel.williams@atlasschool.com>
2025-10-12 17:56:42 -0500

    pycodestyle validation

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,11 +9,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class inherits from Auth - for Basic Authentication"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
         Extracts Base64 part from Basic Auth header
         """
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if (authorization_header is None or
+                not isinstance(authorization_header, str)):
             return None
         if not authorization_header.startswith("Basic "):
             return None
